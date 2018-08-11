b128fa821c0fce4de4a5810b814646d53557ae07
Pavel Koka <pavelkoka@gmail.com>
2018-08-11 16:35:18 +0300

    Initial Commit

---
+++ KPIStateMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KPIStateMachine
+//
+//  Created by Pavel Koka on 8/11/18.
+//  Copyright © 2018 Pavel Koka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KPIStateMachine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KPIStateMachine
+//
+//  Created by Pavel Koka on 8/11/18.
+//  Copyright © 2018 Pavel Koka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
